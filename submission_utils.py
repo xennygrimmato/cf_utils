b6cfdac561ca0ed890196f6bd8e352d08aed5c57
Vaibhav Tulsyan <vstulsyan@gmail.com>
2020-08-16 16:19:41 +0530

    Add TLE, MLE, CE methods

--- submission_utils.py
+++ submission_utils.py
@@ -1,5 +1,8 @@
 AC = 'OK'
 WRONG_ANSWER = 'WRONG_ANSWER'
+TLE = 'TIME_LIMIT_EXCEEDED'
+MLE = 'MEMORY_LIMIT_EXCEEDED'
+COMPILATION_ERROR = 'COMPILATION_ERROR'
 
 
 def is_ac(submission):
@@ -8,3 +11,19 @@ def is_ac(submission):
 
 def is_wa(submission):
     return submission['verdict'] == WRONG_ANSWER
+
+
+def is_tle(submission):
+    return submission['verdict'] == TLE
+
+
+def is_mle(submission):
+    return submission['verdict'] == MLE
+
+
+def is_ce(submission):
+    return submission['verdict'] == COMPILATION_ERROR
+
+
+def is_incorrect(submission):
+    return is_wa(submission) or is_tle(submission) or is_mle(submission) or is_ce(submission)
