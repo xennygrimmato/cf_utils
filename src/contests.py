abfc9ea0583b57f915edfae2981cfd232a5ece3c
Vaibhav Tulsyan <vstulsyan@gmail.com>
2020-09-05 19:22:23 +0530

    Pre-fetch submissions using ProcessPoolExecutor

--- src/contests.py
+++ src/contests.py
@@ -1,6 +1,9 @@
-import cf_base_class
+import concurrent.futures
 
 from codeforces_api import CodeforcesParser
+
+import cf_base_class
+import submission_utils
 
 
 class ContestHelper(cf_base_class.CFBaseClass):
@@ -24,16 +27,29 @@ class ContestHelper(cf_base_class.CFBaseClass):
 
     def group_submissions_by_author(self, submissions):
         author_to_submission = {}
+        submission_id_to_code = {}
         failed = 0
         succeeded = 0
-        for submission in submissions[:100]:
+        submissions = submissions[:500]
+        for submission in submissions:
             author = self.get_author(submission)
             if author not in author_to_submission:
                 author_to_submission[author] = []
             author_to_submission[author].append(submission)
+
+            submission_id_to_code[submission['id']] = ''
+
+        # Get source code for all submissions using ProcessPoolExecutor.
+        # The assumption is that there won't be thread-safety issues as all submissions
+        # have unique IDs.
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for submission, code in zip(submissions, executor.map(self.get_code, submissions)):
+                submission_id_to_code[submission['id']] = code
+
         for author in author_to_submission:
             try:
-                problemwise_mapping = self.get_submissions_of_author_grouped_by_problem(author_to_submission[author])
+                problemwise_mapping = self.get_submissions_of_author_grouped_by_problem(author_to_submission[author],
+                                                                                        submission_id_to_code)
                 author_to_submission[author] = problemwise_mapping
                 succeeded += 1
             except ValueError as ve:
@@ -41,10 +57,14 @@ class ContestHelper(cf_base_class.CFBaseClass):
         print("success: {0}, failure: {1}".format(succeeded, failed))
         return author_to_submission
 
-    def get_code(self, contest_id, submission_id):
-        return self.cf_parser.get_solution(contest_id=contest_id, submit_id=submission_id)
+    def get_code(self, submission):
+        try:
+            return self.cf_parser.get_solution(contest_id=submission['problem']['contestId'],
+                                               submit_id=submission['id'])
+        except:
+            return ''
 
-    def get_submissions_of_author_grouped_by_problem(self, soafc):
+    def get_submissions_of_author_grouped_by_problem(self, soafc, submission_id_to_code):
         """Given all submissions made by an author in a contest, group them by problem ID.
 
         Args:
@@ -53,21 +73,47 @@ class ContestHelper(cf_base_class.CFBaseClass):
         Returns:
             - Dict[Problem, List[Submission]]: Mapping from problem to all submissions for that problem
         """
+        # Add code to submission object
         problem_submission_map = {}
         for submission in soafc:
-            sub_id = submission['id']
             prob_id = submission['problem']['index']
-            code = self.get_code(submission['problem']['contestId'], sub_id)
             if prob_id not in problem_submission_map:
                 problem_submission_map[prob_id] = []
             problem_submission_map[prob_id].append({'submission': submission,
-                                                    'code': code})
+                                                    'code': submission_id_to_code[submission['id']]})
+            del submission_id_to_code[submission['id']]
         return problem_submission_map
+
+    def get_incorrect_correct_pairs(self, submissions_by_author_for_problem):
+        if len(submissions_by_author_for_problem) < 2:
+            return None
+        pair = [None, None]  # incorrect, correct
+        submission_not_dict_counter = 0
+        for submission in submissions_by_author_for_problem:
+            if not isinstance(submission, dict):
+                submission_not_dict_counter += 1
+                continue
+            if submission_utils.is_ac(submission):
+                pair[1] = submission
+            elif submission_utils.is_incorrect(submission):
+                pair[0] = submission
+        print("Submission objects that are not dict: {0}".format(submission_not_dict_counter))
+        return pair
+
+    def get_wa_ac_pairs_for_contest(self, contest_id):
+        all_submissions = self.get_submissions(contest_id)['result']
+        author_submission_mapping = self.group_submissions_by_author(all_submissions)
+        wa_ac_pairs = []
+        for author in author_submission_mapping:
+            pair = self.get_incorrect_correct_pairs(author_submission_mapping[author])
+            if (not pair) or (pair[0] is None) or (pair[1] is None):
+                continue
+            wa_ac_pairs.append(pair)
+        return wa_ac_pairs
 
 
 if __name__ == '__main__':
     ch = ContestHelper()
     finished_contests = ch.get_contests(phase='FINISHED')
-    all_submissions = ch.get_submissions(1409)['result']
-    author_submission_mapping = ch.group_submissions_by_author(all_submissions)
-    print(author_submission_mapping)
+    pairs = ch.get_wa_ac_pairs_for_contest(1409)
+    print(pairs)
